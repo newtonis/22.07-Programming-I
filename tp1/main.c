0c4e57f80a1192a01164eecd4516a671783e0950
newtonis <newtonis.penguin@gmail.com>
2017-08-14 18:44:39 -0300

    added tests

--- tp1/main.c
+++ tp1/main.c
@@ -23,7 +23,6 @@ void check(int ans,int id,int expected){
 
 int main(char argc, char *argv[]){
     
-    
     int ans;
     
     char *test1[] = {"-a","b","c","-e","f","-hf","a"};
@@ -50,8 +49,50 @@ int main(char argc, char *argv[]){
     check(ans,5,3);
     
     char *test6[]={"-Hola", "mundo","-hola", "mundo","test","-hola","hola","-hola","test","test"}; // Sucess
-    ans = parseCmdLine( sz(test5), test5, parseCallback,NULL);
+    ans = parseCmdLine( sz(test6), test6, parseCallback,NULL);
     check(ans,6,6);
+    
+    char *test7[]={"parser","--aw","34gy5","ahchehv3","---54d","hu687","hjgdyt"};
+    ans = parseCmdLine( sz(test7), test7, parseCallback,NULL);
+    check(ans,7,5);
+    
+    char *test8[]={"parser","-f","34gy5","-rt"};
+    ans = parseCmdLine( sz(test8), test8, parseCallback,NULL);
+    check(ans,8,-1);
+    
+    
+    
+    /****** 
+    SALIDA DE ARIEL
+    
+    called [a]=[b]
+    called [c]=[(null)]
+    called [e]=[f]
+    called [hf]=[a]
+    Sucess on test 1
+    Sucess on test 2
+    Sucess on test 3
+    Sucess on test 4
+    called [NAME]=[(null)]
+    called [key]=[-key]
+    called [param]=[(null)]
+    Sucess on test 5
+    called [Hola]=[mundo]
+    called [hola]=[mundo]
+    called [test]=[(null)]
+    called [hola]=[hola]
+    called [hola]=[test]
+    called [test]=[(null)]
+    Sucess on test 6
+    called [parser]=[(null)]
+    called [-aw]=[34gy5]
+    called [ahchehv3]=[(null)]
+    called [--54d]=[hu687]
+    called [hjgdyt]=[(null)]
+    Sucess on test 7
+    Sucess on test 8
+
+     */
     
 }
 
